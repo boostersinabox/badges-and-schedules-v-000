e09c4ba750ee2f5659b47f1ee5a86840f9f32db8
Stacy Carr <stacy@boostersinabox.com>
2018-07-09 18:33:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,10 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
+def batch_badge_creator(names)
+  badge_messages = []
+  "Hello, my name is #{name}."
+  names.each {|name| badge_messages <<}
+  badge_messages
+end
+  
